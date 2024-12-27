3aaa39a2914f72d9943612db28a65e69f622aecb
Prashant Rai <prashantrai0628@gmail.com>
2024-12-28 02:06:03 +0530

    updated backend url

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,1 +1,4 @@
-export const BACKEND_URL = "http://localhost:4001/api";
+export const BACKEND_URL = "https://cillyblogapp.onrender.com";
+
+
+//export const BACKEND_URL = "http://localhost:4001/api";
